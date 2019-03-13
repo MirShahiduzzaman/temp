738435a7fa291d03ceff81019b08c95c6c4a7e5c
Jonathan Lin <jlin7500@bths.edu>
2019-03-13 09:17:15 -0400

    rip

---
+++ First Platformer/Assets/2d-mega-pack/Scripts/2D Platformer/Video11/GameMaster.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+
+    void Start()
+    {
+        if(gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer ()
+    {
+        Debug.Log("TODO: Waiting fir spawn sound");
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("TODO: ADD SPAWN PARTICLES");
+    }
+
+    public static void KillPlayer (Player player) {
+		Destroy (player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+	}
+
+}
---
+++ First Platformer/Assets/coinFound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompletePlayerController : MonoBehaviour
+{
+
+  
+
+
+    // Use this for initialization
+    void Start()
+    {
+     
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+    
+}
